239db69573cdaab771c52e008a213a951549dbc4
@hexlet/cli <support@hexlet.io>
2024-09-03 19:04:41 +0300

    submit java-web-ru/flash

---
+++ java-web-ru/flash/src/main/java/exercise/dto/posts/PostsPage.java
@@ -0,0 +1,15 @@
+package exercise.dto.posts;
+
+import exercise.dto.BasePage;
+import exercise.model.Post;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Getter
+public class PostsPage extends BasePage {
+    private List<Post> posts;
+}
+// END

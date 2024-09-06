f189e34f03bd8710352c7a57d113124f01e92e47
@hexlet/cli <support@hexlet.io>
2024-09-06 19:39:55 +0300

    submit java-spring-ru/response-entity

--- java-spring-ru/response-entity/src/main/java/exercise/Application.java
+++ java-spring-ru/response-entity/src/main/java/exercise/Application.java
@@ -1,11 +1,8 @@
 package exercise;
 
-import java.net.URI;
 import java.util.List;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,10 +10,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
-
 import exercise.model.Post;
 
 @SpringBootApplication

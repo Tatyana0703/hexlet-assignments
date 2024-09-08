9d785d1315bbc997b23b6261dd4dbaa78858b843
@hexlet/cli <support@hexlet.io>
2024-09-08 15:59:41 +0300

    submit java-spring-ru/jpa-data

--- java-spring-ru/jpa-data/src/main/java/exercise/controller/PeopleController.java
+++ java-spring-ru/jpa-data/src/main/java/exercise/controller/PeopleController.java
@@ -12,10 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
-
 import java.util.List;
-import java.util.Optional;
-
 import exercise.model.Person;
 
 @RestController

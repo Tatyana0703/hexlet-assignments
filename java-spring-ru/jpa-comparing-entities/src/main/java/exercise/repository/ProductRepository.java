2418cc031fb123c069c525ff3c70f12207193d41
@hexlet/cli <support@hexlet.io>
2024-09-11 18:54:05 +0300

    submit java-spring-ru/jpa-comparing-entities

---
+++ java-spring-ru/jpa-comparing-entities/src/main/java/exercise/repository/ProductRepository.java
@@ -0,0 +1,12 @@
+package exercise.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import exercise.model.Product;
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    List<Product> findByTitleAndPrice(String title, int price);
+}

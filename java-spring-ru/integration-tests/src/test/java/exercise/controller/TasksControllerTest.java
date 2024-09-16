741cca21efb6322b1c3defc3350634fe802f04eb
@hexlet/cli <support@hexlet.io>
2024-09-16 18:59:04 +0300

    submit java-spring-ru/integration-tests

---
+++ java-spring-ru/integration-tests/src/test/java/exercise/controller/TasksControllerTest.java
@@ -0,0 +1,161 @@
+package exercise.controller;
+
+import org.junit.jupiter.api.Test;
+import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.instancio.Instancio;
+import org.instancio.Select;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+
+import java.util.HashMap;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import net.datafaker.Faker;
+import exercise.repository.TaskRepository;
+import exercise.model.Task;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class TasksControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private Faker faker;
+
+    @Autowired
+    private ObjectMapper om;
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @Test
+    public void testWelcomePage() throws Exception {
+        var result = mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        var body = result.getResponse().getContentAsString();
+        assertThat(body).contains("Welcome to Spring!");
+    }
+
+    @Test
+    public void testIndex() throws Exception {
+        var result = mockMvc.perform(get("/tasks"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        var body = result.getResponse().getContentAsString();
+        assertThatJson(body).isArray();
+    }
+
+    // BEGIN
+    @Test
+    public void testUpdate() throws Exception {
+        var task = Instancio.of(Task.class)
+                .ignore(Select.field(Task::getId))
+                .ignore(Select.field(Task::getUpdatedAt))
+                .ignore(Select.field(Task::getCreatedAt))
+                .supply(Select.field(Task::getTitle), () -> faker.lorem().word())
+                .supply(Select.field(Task::getDescription), () -> faker.lorem().paragraph())
+                .create();
+        taskRepository.save(task);
+
+        var data = new HashMap<>();
+        data.put("title", faker.lorem().word());
+        data.put("description", faker.lorem().paragraph());
+
+        var request = put("/tasks/" + task.getId())
+                .contentType(MediaType.APPLICATION_JSON)
+                // ObjectMapper конвертирует Map в JSON
+                .content(om.writeValueAsString(data));
+
+        mockMvc.perform(request)
+                .andExpect(status().isOk());
+
+        task = taskRepository.findById(task.getId()).get();
+        assertThat(task.getTitle()).isEqualTo(data.get("title"));
+        assertThat(task.getDescription()).isEqualTo(data.get("description"));
+        assertThat(task.getCreatedAt()).isNotNull();
+        assertThat(task.getUpdatedAt()).isNotNull();
+    }
+
+    @Test
+    public void testCreate() throws Exception {
+        HashMap<String, String> data = new HashMap<>();
+        data.put("title", faker.lorem().word());
+        data.put("description", faker.lorem().paragraph());
+
+        var request = post("/tasks")
+                .contentType(MediaType.APPLICATION_JSON)
+                // ObjectMapper конвертирует Map в JSON
+                .content(om.writeValueAsString(data));
+
+        mockMvc.perform(request)
+                .andExpect(status().isCreated());
+
+        var task = taskRepository.findByTitle(data.get("title")).get();
+        assertThat(task.getId()).isNotZero();
+        assertThat(task.getDescription()).isEqualTo(data.get("description"));
+        assertThat(task.getCreatedAt()).isNotNull();
+        assertThat(task.getUpdatedAt()).isNotNull();
+    }
+
+    @Test
+    public void testShow() throws Exception {
+        HashMap<String, String> data = new HashMap<>();
+        data.put("title", faker.lorem().word());
+        data.put("description", faker.lorem().paragraph());
+
+        var task = Instancio.of(Task.class)
+                .ignore(Select.field(Task::getId))
+                .ignore(Select.field(Task::getUpdatedAt))
+                .ignore(Select.field(Task::getCreatedAt))
+                .supply(Select.field(Task::getTitle), () -> data.get("title"))
+                .supply(Select.field(Task::getDescription), () -> data.get("description"))
+                .create();
+        taskRepository.save(task);
+
+        var result = mockMvc.perform(get("/tasks/" +  + task.getId()))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        var body = result.getResponse().getContentAsString();
+        assertThatJson(body).isNotNull();
+        assertThatJson(body).and(
+                v -> v.node("id").isEqualTo(task.getId()),
+                v -> v.node("title").isEqualTo(data.get("title")),
+                v -> v.node("description").isEqualTo(data.get("description")),
+                v -> v.node("createdAt").isNotNull(),
+                v -> v.node("updatedAt").isNotNull()
+        );
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+        var task = Instancio.of(Task.class)
+                .ignore(Select.field(Task::getId))
+                .ignore(Select.field(Task::getUpdatedAt))
+                .ignore(Select.field(Task::getCreatedAt))
+                .supply(Select.field(Task::getTitle), () -> faker.lorem().word())
+                .supply(Select.field(Task::getDescription), () -> faker.lorem().paragraph())
+                .create();
+        taskRepository.save(task);
+
+        mockMvc.perform(delete("/tasks/" +  + task.getId()))
+                .andExpect(status().isOk());
+
+        assertThat(taskRepository.findById(task.getId())).isEmpty();
+    }
+    // END
+}

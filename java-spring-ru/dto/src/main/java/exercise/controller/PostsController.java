d6158d4a34bb26a87e34a970a7d2a9c6c2ac5a41
@hexlet/cli <support@hexlet.io>
2024-09-21 11:04:35 +0300

    submit java-spring-ru/dto

--- java-spring-ru/dto/src/main/java/exercise/controller/PostsController.java
+++ java-spring-ru/dto/src/main/java/exercise/controller/PostsController.java
@@ -7,15 +7,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
-
 import java.util.ArrayList;
 import java.util.List;
-
 import exercise.model.Post;
 import exercise.repository.PostRepository;
 import exercise.exception.ResourceNotFoundException;
 import exercise.dto.PostDTO;
-import exercise.dto.CommentDTO;
 
 // BEGIN
 @RestController
@@ -34,7 +31,8 @@ public class PostsController {
         List<Comment> comments = commentRepository.findAll();
 
         List<PostDTO> dtoPosts = new ArrayList<>();
-        posts.forEach(v -> dtoPosts.add(PostDTO.getInstance(v, comments.stream().filter(c -> c.getPostId() == v.getId()).toList())));
+        posts.forEach(v -> dtoPosts.add(
+                PostDTO.getInstance(v, comments.stream().filter(c -> c.getPostId() == v.getId()).toList())));
 
         return dtoPosts;
     }

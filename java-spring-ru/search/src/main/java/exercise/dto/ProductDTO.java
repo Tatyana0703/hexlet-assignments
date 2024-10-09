dc116b98a723df0688b1a050ca77ce5600950335
@hexlet/cli <support@hexlet.io>
2024-10-09 20:00:05 +0300

    submit java-spring-ru/search

---
+++ java-spring-ru/search/src/main/java/exercise/dto/ProductDTO.java
@@ -0,0 +1,21 @@
+package exercise.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+public class ProductDTO {
+    private Long id;
+    private Long categoryId;
+    private  String categoryName;
+    private String title;
+    private int price;
+    private double rating;
+//    private LocalDate createdAt;
+    private String createdAt;
+//    private LocalDate updatedAt;
+    private String updatedAt;
+}

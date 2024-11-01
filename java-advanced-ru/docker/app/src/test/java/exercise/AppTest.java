0496ef571b54ee0504274efe1a79e227dc865c9e
@hexlet/cli <support@hexlet.io>
2024-11-02 01:02:17 +0300

    submit java-advanced-ru/docker

---
+++ java-advanced-ru/docker/app/src/test/java/exercise/AppTest.java
@@ -0,0 +1,34 @@
+package exercise;
+
+import org.springframework.beans.factory.annotation.Value;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+//import org.springframework.boot.web.server.LocalServerPort;
+
+//@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@SpringBootTest
+public class AppTest {
+
+//    @LocalServerPort
+//    @Value("${server.port}")
+//    private int port;
+
+//    @Autowired
+//    private TestRestTemplate restTemplate;
+
+    @Test
+    public void testRootPage() throws Exception {
+//        String body = this.restTemplate.getForObject(
+//                "http://localhost:" + port + "/",
+//                String.class
+//        );
+//        assertThat(body).contains("Great! We start Spring Boot application in Docker");
+    }
+}
+

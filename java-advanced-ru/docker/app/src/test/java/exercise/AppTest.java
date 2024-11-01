99e59f8544ca575be821a690f65f3e73589b5e0b
@hexlet/cli <support@hexlet.io>
2024-11-02 01:11:33 +0300

    submit java-advanced-ru/docker

--- java-advanced-ru/docker/app/src/test/java/exercise/AppTest.java
+++ java-advanced-ru/docker/app/src/test/java/exercise/AppTest.java
@@ -1,14 +1,14 @@
 package exercise;
 
-import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.beans.factory.annotation.Value;
 
-import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.Assertions.assertThat;
+//import org.junit.jupiter.api.Test;
+//import static org.assertj.core.api.Assertions.assertThat;
 
-import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.web.client.TestRestTemplate;
+//import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+//import org.springframework.boot.test.web.client.TestRestTemplate;
 //import org.springframework.boot.web.server.LocalServerPort;
 
 //@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
